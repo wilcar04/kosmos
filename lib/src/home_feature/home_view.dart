873e3b6b53d62e94bc60fc9294ca9c773174c914
Wilson <wilson.santiago.car@gmail.com>
2025-03-25 22:40:54 -0500

    added primary color

--- lib/src/home_feature/home_view.dart
+++ lib/src/home_feature/home_view.dart
@@ -10,14 +10,34 @@ class HomeView extends StatelessWidget {
             padding:
                 const EdgeInsets.symmetric(horizontal: 16.0, vertical: 48.0),
             child:
-                Column(mainAxisAlignment: MainAxisAlignment.start, children: [
-              Text(
-                'Hi, Wilson',
-                style: Theme.of(context).textTheme.headlineSmall,
+                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+              Row(
+                spacing: 5,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Icon(
+                    Icons.public,
+                    color: Theme.of(context).primaryColor,
+                  ),
+                  Text(
+                    'Hi, Wilson',
+                    style: Theme.of(context).textTheme.headlineSmall,
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: 20,
               ),
               Text(
                 'Discover the world through data',
                 style: Theme.of(context).textTheme.headlineLarge,
+              ),
+              SizedBox(
+                height: 60,
+              ),
+              Text(
+                'Popular countries',
+                style: Theme.of(context).textTheme.headlineSmall,
               ),
             ])));
   }
